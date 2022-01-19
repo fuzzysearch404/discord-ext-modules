d787b43f262e6dbe0ce73d1aff1606dd94710af5
fuzzysearch404 <roberts.ziedins@inbox.lv>
2022-01-19 02:59:08 +0200

    Add package to packages in setup

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.1.0",
     description="Extension for modular application command clients",
     author="fuzzysearch404",
+    packages=["discord.ext.modules"],
     license="MIT",
     url="https://github.com/fuzzysearch404/discord-ext-modules"
 )
